e6868e3b230dbb8eee106ac998859e873535c44e
Anshika Agarwal <143865467+Anshika0504@users.noreply.github.com>
2025-02-05 21:36:40 +0530

    Add files via upload

--- builtin.R
+++ builtin.R
@@ -1,35 +1,72 @@
-#r built in function
-x<-5.3
-print(abs(x))
-print(sqrt(x))
-print(floor(x))
-print(ceiling(x))
-y<-c(1.5,5.6,8.6)
-print(trunc(y))
 
-z<-5
-print(sin(z))
-print(log(z))
-print(exp(z))
+#data structure
+#vector
+#array
+#list
+#data frames
+#elements of vector are known as components
+#length-finds no of elements in the vector
+#vector->atomic and list
 
-#string function
-#indexes starts from 1 in r programming
-a<-"anshika"
-print(substr(a,1,6))
-print(toupper(a))
-print(tolower(a))
+#creation of vector
 
-s2<-c("shimona","priyanshi","anshika","tanmay","ankush")
-pat<-'^an'
-print(grep(pat,s2))
+c1<-c(1,2,3,4,5)
+c1
 
-#statistical functions
-a1<-c(1:5)
-print(a1)
-su<-sum(a1)
-print(su)
-print(max(a1))
-print(min(a1))
-print(range(a1))
+#using colon operator
+b<--3:5
+b
+typeof(b)
 
+sq<-seq(1,4,length.out=5)
+sq
+#numeric vector
+numv<-c(12.3,52.2,6.23,56)
+numv
+class(numv)
+print(numv[3])
 
+intv<-c(5,8,9,7,6)
+typeof(intv)
+int3<-as.integer(intv)
+class(int3)
+
+#character vector
+charv<-c("anshika","akhilseh","babita","priyanshi")
+typeof(charv)
+
+#logical vector
+
+# acessing the vectors elements
+
+#by indexing [] indexing starts from 1 in R programming
+
+char_vec<-c("ram"=12,"shyam"=32,"mohan"=31)
+char_vec["shyam"]
+char_vec[1]
+
+a1<-c(1,2,3,4,5,6)
+a1[c(TRUE,FALSE,FALSE,TRUE,TRUE,FALSE)]
+a1
+
+#vector operation
+a1<-c(1,2,3,4,5)
+a4<-c(1,2,3,4,5)
+a2<-c("anshika","akhilesh","babita","priyanshi","shimona")
+a3<-c(a1,a2)
+a5<-(a1+a4)
+a5
+
+a2<-c("anshika","akhilesh","babita","priyanshi","shimona")
+a2[3]
+a2[-1]#print all except that index element
+a2[2:4]
+a2[2:3]
+a2[c(2,3,2)]
+
+#naming of the vector
+z<-c("ram","mohan","sohan")
+z
+names(z)=c("y1","y2","y3")
+z
+z["y3"]
